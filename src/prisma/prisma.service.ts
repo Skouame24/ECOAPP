99598f4277a19b0a2bb2de34182bb1085d4cdcfa
Sevan AKOUMIA <sevanakoumia@MacBook-Air-de-Ibrahim.local>
2025-02-09 20:06:53 +0000

    gestion de build

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,15 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { execSync } from 'child_process';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+    try {
+      execSync('npx prisma generate', { stdio: 'inherit' });
+    } catch (error) {
+      console.error('Error running prisma generate:', error);
+    }
     await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
   }
 }
